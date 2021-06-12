d1b57940b448ff399f26b54986e927f11d4738f4
Roy Hashimoto <roy@shoestringresearch.com>
2021-06-12 07:20:20 -0700

    Create project.

---
+++ test/foo.test.js
@@ -0,0 +1,5 @@
+describe('foo', function() {
+  it('tests something', async function() {
+    console.log('hello');
+  });
+});
---
+++ web-test-runner.config.mjs
@@ -0,0 +1,5 @@
+export default {
+  files: ['test/*.test.js'],
+  nodeResolve: true,
+  watch: true
+}
